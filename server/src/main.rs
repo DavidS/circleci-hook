5f4c6a4202d49f937b68a3623eae5e5149a6dd65
David Schmitt <david@black.co.at>
2022-08-27 22:17:26 +0100

    chore: start experimenting with otel

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,4 +1,5 @@
 use axum::{
+    extract::State,
     http::StatusCode,
     response::IntoResponse,
     routing::{get, post},
@@ -8,13 +9,24 @@ use axum::{
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 
+use opentelemetry::{
+    global, sdk::export::trace::stdout, sdk::trace::Tracer, trace::Tracer as OtherTracer,
+};
+
 mod structs;
 
+#[derive(Clone)]
+struct AppState {
+    tracer: Tracer,
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    let app = Router::new()
+    let tracer = stdout::new_pipeline().install_simple();
+    let state = AppState { tracer };
+
+    let app = Router::with_state(state)
         .route("/", get(root))
         .route("/", post(hook_handler));
 
@@ -35,7 +47,12 @@ async fn root() -> &'static str {
 //     "Hello, Mikey and backendsouls!"
 // }
 
-async fn hook_handler(Json(payload): Json<structs::WebhookPayload>) -> &'static str {
-    println!("{:#?}", payload);
+async fn hook_handler(
+    State(state): State<AppState>,
+    Json(payload): Json<structs::WebhookPayload>,
+) -> &'static str {
+    state.tracer.in_span("test", |cx| {
+        println!("{:#?}", payload);
+    });
     "Hello, Mikey and backendsouls!"
 }
